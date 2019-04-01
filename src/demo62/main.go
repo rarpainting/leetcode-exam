2a0666ede8775fa800e7814cd320c97f2b7b41be
Rarpainting <zhandequan@yahoo.com>
2019-04-01 23:17:03 +0800

    update

--- src/demo62/main.go
+++ src/demo62/main.go
@@ -9,7 +9,11 @@ func main() {
 }
 
 func uniquePaths(m int, n int) int {
+	if m < 1 || n < 1 {
+		return 0
+	}
 	dp := make([]int, n)
+	dp[0] = 1
 	for i := 1; i < m; i++ {
 		for j := 1; j < n; j++ {
 			dp[j] += dp[j-1]
