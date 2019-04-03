a91a7dbb6480b1b932d16a49876cf3a2d8b4c24c
ZhanDequan <zhandequanla00@gmail.com>
2019-04-03 09:43:36 +0800

    update 22[括号生成]

--- src/demo22/main.go
+++ src/demo22/main.go
@@ -1,6 +1,40 @@
 package main
 
-// 怎么把 n 个节点挂在树上
-func generateParenthesis(n int) []string {
+import (
+	"flag"
+	"fmt"
+)
 
+var (
+	num = flag.Int("num", 1, "")
+)
+
+func main() {
+	flag.Parse()
+	out := generateParenthesis(*num)
+	fmt.Println(out)
+}
+
+// DFS 遍历某深度的树
+func generateParenthesis(n int) []string {
+	out := generateParenthesisDFS(n, n, "")
+	return out
+}
+
+func generateParenthesisDFS(left, right int, symbol string) (out []string) {
+	// 要求必须先排了左符号, 再排右符号
+	// if left > right {
+	// 	return
+	// }
+	if left == 0 && right == 0 { // 在节点尾部
+		out = append(out, symbol)
+	} else {
+		if left > 0 {
+			out = append(out, generateParenthesisDFS(left-1, right, symbol+"(")...)
+		}
+		if right > 0 {
+			out = append(out, generateParenthesisDFS(left, right-1, symbol+")")...)
+		}
+	}
+	return
 }
