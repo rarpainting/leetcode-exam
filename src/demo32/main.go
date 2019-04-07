29f31423db26355922be1bcd165524a077597b1f
Rarpainting <zhandequan@yahoo.com>
2019-04-07 21:37:08 +0800

    update 32[最长有效括号]

--- src/demo32/main.go
+++ src/demo32/main.go
@@ -24,21 +24,26 @@ func longestValidparentheses(s string) (max int) {
 		lstack := len(stack)
 		if sbyte[i] == byte(')') {
 			if lstack == 0 {
-				// 结束
+				// 下一次
 				start = i + 1
 				continue
 			}
 			// pop
 			// pre := stack[lstack-1]
 			stack = stack[:lstack-1]
+
 			if len(stack) == 0 {
+				// 只修改 max
 				if max < i-start+1 {
 					max = i - start + 1
 				}
 			} else {
 				lstack = len(stack)
+
+				// pop
 				pre := stack[lstack-1]
-				stack = stack[:lstack-1]
+				// stack = stack[:lstack-1]
+
 				if max < i-pre {
 					max = i - pre
 				}
@@ -49,4 +54,27 @@ func longestValidparentheses(s string) (max int) {
 	}
 
 	return
+}
+
+func longestValidParenthesesDP(s string) (maxLen int) {
+	n := len(s)
+	dp := make([]int, n)
+	for i := 2; i <= n; i++ {
+		j := i - 2 - dp[i-1]
+		if s[i-1] == '(' || j < 0 || s[j] == ')' {
+			dp[i] = 0
+		} else {
+			dp[i] = dp[i-1] + 2 + dp[j]
+			maxLen = max(maxLen, dp[i])
+		}
+	}
+
+	return
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
