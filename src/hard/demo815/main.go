b3d9f09e8d9364a0c4f8ad0760684e1b6c49bdfc
Rarpainting <zhandequan@yahoo.com>
2019-06-02 18:46:32 +0800

    update 815[bus stop]

--- src/hard/demo815/main.go
+++ src/hard/demo815/main.go
@@ -30,11 +30,88 @@ func main() {
 
 }
 
+type Bus struct {
+	BusId   int
+	BusStop []int
+}
+
+// Dijkstra
+// 由位置 A 到位置 B 的路途中
 // 求出最少乘坐的公交车数量, 而不是最短的路径...
+// routes[i] -- 公交车索引
+// route[i][j] -- 该公交车 i 可到的站台号
 func numBusesToDestination(routes [][]int, S int, T int) int {
 	if S == T {
 		return 0
 	}
 
-	return -1
+	// 能到达该站点的公交车索引
+	// 即 站台 busStop[i] 为 结点, 公交车 routes[j] 为线的最短路径
+	busStop := make(map[int][]Bus)
+	for i, v := range routes {
+		for si, sv := range v {
+			busStop[sv] = append(busStop[sv], Bus{
+				BusId:   i,
+				BusStop: append(v[:si], v[si+1:]...),
+			})
+		}
+	}
+
+	// 一开始就有序, 我排序你 mua
+	stack := []int{S}                // [站点]栈
+	visitedSet := make(map[int]bool) // 已遍历的[站点]
+	res := 0
+
+	for {
+		if len(stack) == 0 {
+			return -1
+		}
+
+		res++
+
+		for i := 0; i < len(stack); i++ {
+			// pop
+			t := stack[0]
+			stack = stack[1:]
+
+			for _, v := range busStop[t] {
+				for _, stopv := range routes[v.BusId] {
+					if _, ok := visitedSet[stopv]; ok {
+						continue
+					}
+					visitedSet[stopv] = true
+					if stopv == T {
+						return res
+					}
+					stack = append(stack, stopv)
+				}
+			}
+		}
+	}
+
+	// return -1
+}
+
+// 排序
+func sort(src []int) []int {
+	if len(src) < 2 {
+		return src
+	} else if len(src) < 3 {
+		if src[0] > src[1] {
+			return []int{src[1], src[0]}
+		}
+		return src
+	}
+
+	dist, o := []int{}, src[0]
+	l, g := []int{}, []int{}
+	for _, v := range src[1:] {
+		if v < o {
+			l = append(l, v)
+		} else {
+			g = append(g, v)
+		}
+	}
+	dist = append(sort(l), o)
+	return append(dist, sort(g)...)
 }
