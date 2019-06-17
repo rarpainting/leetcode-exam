662e5273ea2b245c84d8295c1c27971f2dce4c2b
ZhanDequan <zhandequanla00@gmail.com>
2019-06-17 16:53:36 +0800

    Update

--- src/other/double-array-trie/main.go
+++ src/other/double-array-trie/main.go
@@ -4,19 +4,43 @@ func main() {
 }
 
 func NewDoubelArrayTrie() *DoubleArrayTrie {
-	return &DoubleArrayTrie{}
+	return &DoubleArrayTrie{
+		Base:  append([]rune{}, 1),
+		Check: append([]rune{}, 0),
+
+		// 父节点无数据, 同时最后的节点必须挂一个空的节点
+		Node: Node{
+			Depth: 0,
+			Children: map[rune]Node{
+				0: Node{
+					Depth: 1,
+				},
+			},
+		},
+	}
+}
+
+func (dat *DoubleArrayTrie) Build(keys [][]rune) error {
+	if len(keys) == 0 {
+		return nil
+	}
+
+	return nil
 }
 
 type DoubleArrayTrie struct {
-	Check []rune
 	Base  []rune
+	Check []rune
 
 	Node
 }
 
 type Node struct {
-	Code  int // 编码
-	Depth int // 树深
-	Left  int // 范围
-	Right int
+	// Code  int    // 编码
+	Depth int    // 树深
+	Range [2]int // 范围
+	// Left  int // 范围
+	// Right int
+
+	Children map[rune]Node
 }
